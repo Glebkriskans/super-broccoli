86414a6d863a342815a2f1ced5c70b15a77e59b3
Gleb Krishkans <92617118+Glebkriskans@users.noreply.github.com>
2023-02-22 16:00:19 +0500

    Add files via upload

---
+++ WpfApp7/WpfApp7/ApplicationData/AppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp7.ApplicationData
+{
+    public static class AppData
+    {
+        public static User7Entities db = new User7Entities();
+    }
+}
